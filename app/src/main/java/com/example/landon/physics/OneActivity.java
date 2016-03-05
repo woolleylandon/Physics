5d879c95d6b50192593a03ed785970b595ed4474
woolleylandon <woolleylandon@gmail.com>
2016-03-05 01:42:24 -0600

    Starting to add math intergration to activity one

--- app/src/main/java/com/example/landon/physics/OneActivity.java
+++ app/src/main/java/com/example/landon/physics/OneActivity.java
@@ -5,6 +5,11 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.landon.physics.logic.MCV;
+import com.example.landon.physics.logic.Measure;
 
 public class OneActivity extends AppCompatActivity {
 
@@ -29,5 +34,19 @@ public class OneActivity extends AppCompatActivity {
     }
 
     public void oneClick(View v) {
+        /*EditText x0Text = (EditText) findViewById(R.id.x0Text);
+        EditText xfText = (EditText) findViewById(R.id.xfText);
+        EditText vText  = (EditText) findViewById(R.id.vText);
+        EditText tText  = (EditText) findViewById(R.id.tText);
+
+        double x0TextInt = Double.parseDouble(x0Text.getText().toString());
+        double xfTextInt = Double.parseDouble(xfText.getText().toString());
+        double vTextInt  = Double.parseDouble(vText.getText().toString());
+        double tTextInt  = Double.parseDouble(tText.getText().toString());
+
+        MCV sys = new MCV(new Measure(x0TextInt, "m"),
+                new Measure(xfTextInt, "m"),
+                new Measure(vTextInt, "m"),
+                new Measure(tTextInt, "m"));*/
     }
 }
