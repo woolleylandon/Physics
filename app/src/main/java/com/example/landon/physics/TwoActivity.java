306855a87af97dbbbd0b07c448a6fb5c43de7e14
marcorosas1991 <marco.rosas@outlook.com>
2016-03-07 08:39:38 -0700

    Add functionality to Activity_Two controller.

--- app/src/main/java/com/example/landon/physics/TwoActivity.java
+++ app/src/main/java/com/example/landon/physics/TwoActivity.java
@@ -5,6 +5,11 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import com.example.landon.physics.logic.MCA;
+import com.example.landon.physics.logic.MCV;
+import com.example.landon.physics.logic.Measure;
 
 public class TwoActivity extends AppCompatActivity {
 
@@ -29,5 +34,62 @@ public class TwoActivity extends AppCompatActivity {
     }
 
     public void twoClick(View v) {
+        EditText s0Text = (EditText) findViewById(R.id.mca_s0_txt);
+        EditText sfText = (EditText) findViewById(R.id.mca_sf_txt);
+        EditText v0Text  = (EditText) findViewById(R.id.mca_v0_txt);
+        EditText vfText  = (EditText) findViewById(R.id.mca_vf_txt);
+        EditText aText  = (EditText) findViewById(R.id.mca_a_txt);
+        EditText tText  = (EditText) findViewById(R.id.mca_t_txt);
+
+
+        Measure s0 = null;
+        Measure sf = null;
+        Measure v0 = null;
+        Measure vf = null;
+        Measure a = null;
+        Measure t = null;
+
+        if (s0Text.getText().toString().equals("") == false) {
+            s0 = new Measure(Double.parseDouble(s0Text.getText().toString()), "m");
+            System.out.println("S0 is NOT empty: " + s0Text.getText().toString());
+        } else {
+            System.out.println("S0 IS empty: " + s0Text.getText().toString());
+        }
+
+        if (sfText.getText().toString().equals("") == false) {
+            sf = new Measure(Double.parseDouble(sfText.getText().toString()), "m");
+            System.out.println("Sf is NOT empty: " + sfText.getText().toString());
+        } else {
+            System.out.println("Sf IS empty: " + sfText.getText().toString());
+        }
+
+        if (v0Text.getText().toString().equals("") == false) {
+            v0 = new Measure(Double.parseDouble(v0Text.getText().toString()), "m");
+        }
+
+        if (vfText.getText().toString().equals("") == false) {
+            vf = new Measure(Double.parseDouble(vfText.getText().toString()), "m");
+        }
+
+        if (aText.getText().toString().equals("") == false) {
+            a = new Measure(Double.parseDouble(aText.getText().toString()), "m");
+        }
+
+        if (tText.getText().toString().equals("") == false) {
+            t = new Measure(Double.parseDouble(tText.getText().toString()), "m");
+        }
+
+        MCA system = new MCA(s0, sf, v0, vf, a, t);
+
+        if (system.solveSystem()) {
+            s0Text.setText(system.getS0().getMagnitude() + "");
+            sfText.setText(system.getSf().getMagnitude() + "");
+            v0Text.setText(system.getV0().getMagnitude() + "");
+            vfText.setText(system.getVf().getMagnitude() + "");
+            aText.setText(system.getA().getMagnitude() + "");
+            tText.setText(system.getT().getMagnitude() + "");
+        } else {
+            // say in step by step that system cannot be solved
+        }
     }
 }
