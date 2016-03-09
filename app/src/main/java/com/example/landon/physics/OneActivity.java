3068e33ead192035c89217be5858c322275dc43d
Landon Woolley <woolleylandon@gmail.com>
2016-03-09 01:39:02 -0700

    Almost finished logic integration on activity one

--- app/src/main/java/com/example/landon/physics/OneActivity.java
+++ app/src/main/java/com/example/landon/physics/OneActivity.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
-import android.widget.TextView;
-
 import com.example.landon.physics.logic.MCV;
 import com.example.landon.physics.logic.Measure;
+import android.support.v7.app.AppCompatActivity;
 
 public class OneActivity extends AppCompatActivity {
 
@@ -33,20 +31,54 @@ public class OneActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
     }
 
-    public void oneClick(View v) {
-        /*EditText x0Text = (EditText) findViewById(R.id.x0Text);
+    public void oneClick(View view) {
+        EditText x0Text = (EditText) findViewById(R.id.x0Text);
         EditText xfText = (EditText) findViewById(R.id.xfText);
-        EditText vText  = (EditText) findViewById(R.id.vText);
-        EditText tText  = (EditText) findViewById(R.id.tText);
+        EditText vText = (EditText) findViewById(R.id.vText);
+        EditText tText = (EditText) findViewById(R.id.tText);
 
-        double x0TextInt = Double.parseDouble(x0Text.getText().toString());
-        double xfTextInt = Double.parseDouble(xfText.getText().toString());
-        double vTextInt  = Double.parseDouble(vText.getText().toString());
-        double tTextInt  = Double.parseDouble(tText.getText().toString());
+        Measure x0 = null;
+        Measure xf = null;
+        Measure v = null;
+        Measure t = null;
 
-        MCV sys = new MCV(new Measure(x0TextInt, "m"),
-                new Measure(xfTextInt, "m"),
-                new Measure(vTextInt, "m"),
-                new Measure(tTextInt, "m"));*/
+        if (!x0Text.getText().toString().equals("")) {
+            x0 = new Measure(Double.parseDouble(x0Text.getText().toString()), "m");
+            System.out.println("S0 is NOT empty: " + x0Text.getText().toString());
+        } else {
+            System.out.println("S0 IS empty: " + x0Text.getText().toString());
+        }
+
+        if (!xfText.getText().toString().equals("")) {
+            xf = new Measure(Double.parseDouble(xfText.getText().toString()), "m");
+            System.out.println("S0 is NOT empty: " + xfText.getText().toString());
+        } else {
+            System.out.println("S0 IS empty: " + xfText.getText().toString());
+        }
+
+        if (!vText.getText().toString().equals("")) {
+            v = new Measure(Double.parseDouble(vText.getText().toString()), "m");
+            System.out.println("S0 is NOT empty: " + vText.getText().toString());
+        } else {
+            System.out.println("S0 IS empty: " + vText.getText().toString());
+        }
+
+        if (!tText.getText().toString().equals("")) {
+            t = new Measure(Double.parseDouble(tText.getText().toString()), "m");
+            System.out.println("S0 is NOT empty: " + tText.getText().toString());
+        } else {
+            System.out.println("S0 IS empty: " + tText.getText().toString());
+        }
+
+        MCV system = new MCV(x0, xf, v, t);
+
+        if (system.solveSystem()) {
+            x0Text.setText(system.getX0().getMagnitude() + "");
+            xfText.setText(system.getXf().getMagnitude() + "");
+            vText.setText(system.getV().getMagnitude() + "");
+            tText.setText(system.getT().getMagnitude() + "");
+        } else {
+            // say in step by step that system cannot be solved
+        }
     }
 }
--- app/src/main/java/com/example/landon/physics/TwoActivity.java
+++ app/src/main/java/com/example/landon/physics/TwoActivity.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
-
 import com.example.landon.physics.logic.MCA;
-import com.example.landon.physics.logic.MCV;
 import com.example.landon.physics.logic.Measure;
+import android.support.v7.app.AppCompatActivity;
 
 public class TwoActivity extends AppCompatActivity {
 
@@ -41,7 +39,6 @@ public class TwoActivity extends AppCompatActivity {
         EditText aText = (EditText) findViewById(R.id.mca_a_txt);
         EditText tText = (EditText) findViewById(R.id.mca_t_txt);
 
-
         Measure s0 = null;
         Measure sf = null;
         Measure v0 = null;
@@ -49,33 +46,33 @@ public class TwoActivity extends AppCompatActivity {
         Measure a = null;
         Measure t = null;
 
-        if (s0Text.getText().toString().equals("") == false) {
+        if (!s0Text.getText().toString().equals("")) {
             s0 = new Measure(Double.parseDouble(s0Text.getText().toString()), "m");
             System.out.println("S0 is NOT empty: " + s0Text.getText().toString());
         } else {
             System.out.println("S0 IS empty: " + s0Text.getText().toString());
         }
 
-        if (sfText.getText().toString().equals("") == false) {
+        if (!sfText.getText().toString().equals("")) {
             sf = new Measure(Double.parseDouble(sfText.getText().toString()), "m");
             System.out.println("Sf is NOT empty: " + sfText.getText().toString());
         } else {
             System.out.println("Sf IS empty: " + sfText.getText().toString());
         }
 
-        if (v0Text.getText().toString().equals("") == false) {
+        if (!v0Text.getText().toString().equals("")) {
             v0 = new Measure(Double.parseDouble(v0Text.getText().toString()), "m");
         }
 
-        if (vfText.getText().toString().equals("") == false) {
+        if (!vfText.getText().toString().equals("")) {
             vf = new Measure(Double.parseDouble(vfText.getText().toString()), "m");
         }
 
-        if (aText.getText().toString().equals("") == false) {
+        if (!aText.getText().toString().equals("")) {
             a = new Measure(Double.parseDouble(aText.getText().toString()), "m");
         }
 
-        if (tText.getText().toString().equals("") == false) {
+        if (!tText.getText().toString().equals("")) {
             t = new Measure(Double.parseDouble(tText.getText().toString()), "m");
         }
 
